ed5c4ced588b451d71edeef1d3fa9f09703d5a5b
Enya <Enya@HOME-PC>
2022-08-30 17:39:26 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿static void Print(string msg, int delay)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        Console.WriteLine($"{msg} поток №{Task.CurrentId}");
+        Thread.Sleep(delay);
+    }
+}
+
+var task1 = Task.Run(() => Print("+", 100));
+var task2 = Task.Run(() => Print("-", 500));
+Task.WaitAll(task1, task2);
